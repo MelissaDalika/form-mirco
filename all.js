3dcd88d4244cf603e5897bcd1350a025dc0172f2
Melissa Lopez Valdez <melissa.melisita@gmail.com>
2020-07-23 15:32:51 +0000

    change body-parser

--- all.js
+++ all.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const bodyParser = require('body-parser');
 
+app.use(express.json())
 app.use(express.static("./views"));
 app.use(express.static("./assets"));
 
@@ -14,7 +14,7 @@ app.get("/page", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "/views/formpage.html"));
 });
 
-app.post("/submit", bodyParser.urlencoded(), (req, res) => {
+app.post("/submit",(req, res) => {
   //res.send(req.body);
   res.send();
   console.log(req.body);
