752d6c6443eed08a64a66406870dbe107054c20b
Melissa Lopez Valdez <melissa.melisita@gmail.com>
2020-07-24 10:31:45 +0000

    draft js functions and button submit

--- all.js
+++ all.js
@@ -21,6 +21,44 @@ app.post("/submit",(req, res) => {
 });
 
 
+//INPUT RADIO BUTTONS
+
+function getRadioVal() {
+  let selectedRadioValue;
+  // get list of radio buttons with specified name
+  let radiosArr = document.getElementsByClassName("radioInput");
+
+  // loop through list of radio buttons
+  for (let i = 0; i < radiosArr.length; i++) {
+    if (radiosArr[i].checked) {
+      // radio checked?
+      console.log("getRadioVal sta andando");
+      selectedRadioValue = document.getElementById(`answer${i}label`).innerText; // if so, hold its value in val
+      break; // and break out of for loop
+    }
+  }
+  return selectedRadioValue; // return value of checked radio or undefined if none checked
+}
+
+getRadioVal()
+
+//Bottone Submit form
+document.getElementById("ConfirmData").addEventListener("click", () => {
+  document.getElementById("formName").classList.toggle('d-none');
+  document.getElementById("formName").classList.toggle('d-flex');
+  document.getElementById("optionSelection").classList.toggle('d-none');
+});
+
+//Chiamata POST 
+function postData (name, email, tickets, datein, dateout) {
+  var data = JSON.stringify({
+    "name": name,
+    "email": email,
+    "tickets": tickets,
+    "datein": datein,
+    "dateout": dateout
+  });
+}
 
 
 app.listen(3000, () => console.log("Listening on port 3000"));
