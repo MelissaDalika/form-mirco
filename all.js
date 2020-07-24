33ef105e9bb5163efb38ce3b1235871eb817735d
Melissa Lopez Valdez <melissa.melisita@gmail.com>
2020-07-24 09:41:38 +0000

    image fix

--- all.js
+++ all.js
@@ -20,40 +20,7 @@ app.post("/submit",(req, res) => {
   console.log(req.body);
 });
 
-/*
-//VALIDATE USERNAME AND EMAIL
-const submitBtn = document.getElementById('submit-btn');
 
-const validate = (e) => {
-  e.preventDefault();
-  const username = document.getElementById('username');
-  const emailAddress = document.getElementById('email-address');
-  if (username.value === "") {
-    alert("Please enter your username.");
-    username.focus();
-    return false;
-  }
-    
-  if (emailAddress.value === "") {
-    alert("Please enter your email address.");
-    emailAddress.focus();
-    return false;
-  }
 
-  if (!emailIsValid(emailAddress.value)) {
-    alert("Please enter a valid email address.");
-    emailAddress.focus();
-    return false;
-  }
-  
-  return true; // Can submit the form data to the server
-}
-
-const emailIsValid = email => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-submitBtn.addEventListener('click', validate);
-*/
 
 app.listen(3000, () => console.log("Listening on port 3000"));
